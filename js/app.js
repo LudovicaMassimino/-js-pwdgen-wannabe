441ff2e44da450a8e26c4d850aff503449287a0f
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-06 23:13:18 +0100

    let, const, prompt, concatenated variables

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+console.log ('Ciao a tutti!');
+
+// nome
+let nome = prompt('Qual è il tuo nome?'); // string
+console.log(nome)
+
+// cognome 
+let cognome = prompt('Qual è il tuo cognome?'); // string
+console.log(cognome)
+
+// colore preferito
+let colorePreferito = prompt('Qual è il tuo colore preferito?'); // string
+console.log(colorePreferito)
+
+//numero psw
+let numero = (39); // number
+console.log(numero)
+
+// password
+const password = nome + cognome + colorePreferito + numero ; // string
+console.log('La tua nuova password è: ' + nome + cognome + colorePreferito + numero)
