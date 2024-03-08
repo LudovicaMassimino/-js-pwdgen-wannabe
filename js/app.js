2ba8ebc18d146bec55de99ce1c4a85998326ead4
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-08 19:37:38 +0100

    edit after lesson

--- js/app.js
+++ js/app.js
@@ -2,22 +2,26 @@
 
 console.log ('Ciao a tutti!');
 
-// nome
-let nome = prompt('Qual è il tuo nome?'); // string
+// chiedere il nome
+const nome = prompt('Qual è il tuo nome?'); // string
 console.log(nome)
 
-// cognome 
-let cognome = prompt('Qual è il tuo cognome?'); // string
+// chiedere il cognome 
+const cognome = prompt('Qual è il tuo cognome?'); // string
 console.log(cognome)
 
-// colore preferito
-let colorePreferito = prompt('Qual è il tuo colore preferito?'); // string
+// chiedere il colore preferito
+const colorePreferito = prompt('Qual è il tuo colore preferito?'); // string
 console.log(colorePreferito)
 
-//numero psw
-let numero = (39); // number
+// numero psw scelto da me
+const numero = (39); // number
 console.log(numero)
 
-// password
+// generare password
 const password = nome + cognome + colorePreferito + numero ; // string
-console.log('La tua nuova password è: ' + nome + cognome + colorePreferito + numero)
+console.log('La tua nuova password è: ' + nome + cognome + colorePreferito + numero) // oppure console.log(password)
+
+// bonus: generare un numero random
+const numeroRandom = Math.floor (Math.random (1) * 100) + 1; // number
+console.log(numeroRandom)
