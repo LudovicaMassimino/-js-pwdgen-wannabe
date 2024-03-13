f05a6f3516a86afbdf1604322b74b261bfb9fdc2
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-13 12:54:01 +0100

    edit brackets 18, password + numeroRandom inside

--- js/app.js
+++ js/app.js
@@ -15,13 +15,17 @@ const colorePreferito = prompt('Qual è il tuo colore preferito?'); // string
 console.log(colorePreferito)
 
 // numero psw scelto da me
-const numero = (39); // number
+const numero = 39 ; // number
 console.log(numero)
 
 // generare password
-const password = nome + cognome + colorePreferito + numero ; // string
-console.log('La tua nuova password è: ' + nome + cognome + colorePreferito + numero) // oppure console.log(password)
+// const password = nome + cognome + colorePreferito + numero ; // string
+// console.log('La tua nuova password è: ' + nome + cognome + colorePreferito + numero) // oppure console.log(password)
 
 // bonus: generare un numero random
 const numeroRandom = Math.floor (Math.random (1) * 100) + 1; // number
 console.log(numeroRandom)
+
+// generare password con numero random all'interno
+const password = nome + cognome + colorePreferito + numeroRandom ; // string
+console.log('La tua nuova password è: ' + nome + cognome + colorePreferito + numeroRandom)
